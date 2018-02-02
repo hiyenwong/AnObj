220166900cd9e2be769ffdcf4615cba185449848
Lane391 <robinwong51@qq.com>
2018-02-02 14:17:06 +0800

    array to an-obj

---
+++ src/AnObj.php
@@ -0,0 +1,51 @@
+<?php
+  /**
+  * Array convert to a Anonymous Object
+  */
+
+  namespace hiyen\AnObj;
+
+  class AnObj
+  {
+    protected $methods = array();
+    protected $properties = array();
+
+  	public function __construct(array $options)
+  	{
+
+  		foreach($options as $key => $opt) {
+  			//integer, string, float, boolean, array
+  			if(is_array($opt) || is_scalar($opt)) {
+  				$this->properties[$key] = $opt;
+  				unset($options[$key]);
+  			}
+  		}
+
+  		$this->methods = $options;
+  		 foreach($this->properties as $k => $value) {
+
+         $this->{$k} = $value;
+
+         if (is_array($value))
+           $this->{$k} = new self($value);
+
+       }
+
+  	}
+
+  	public function __call($name, $arguments)
+  	{
+  		$callable = null;
+  		if (array_key_exists($name, $this->methods))
+  			$callable = $this->methods[$name];
+  		elseif(isset($this->$name))
+  			$callable = $this->$name;
+
+  		if (!is_callable($callable))
+  			throw new BadMethodCallException("Method {$name} does not exists");
+
+      $method = $callable->call($this);
+
+  		return call_user_func_array($callable, $arguments);
+  	}
+  }
